d3c389e673783e83a6daa0cb2855173c7794be5d
Anurag Ganji <anuragganji09@gmail.com>
2023-08-03 12:25:48 -0700

    Time: 2 ms (80.75%), Space: 43.5 MB (89.2%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,37 +1,12 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        Set<Integer> set = new HashSet<Integer>();
-
-        //for finding the solution pair
-        for(int i=0; i<nums.length; i++){
-            set.add(nums[i]);
+        Map<Integer, Integer> map = new HashMap();
+        for(int i = 0; i < nums.length; i++){
+            if(map.containsKey(target - nums[i])){
+                return new int[]{i, map.get(target - nums[i])};
+            }
+            map.put(nums[i], i);
         }
-        int first=0,second=0;
-       for(int i=0; i<nums.length; i++){
-           if(set.contains(target-nums[i])){
-               boolean flag = false;
-               //to find the index of the pair
-               for(int j=0;j<nums.length;j++){
-                   //recording the index at the solution pair
-                   if(nums[j]==nums[i] || nums[j]==target-nums[i]){
-                       if(!flag){
-                           first = j;
-                           //for duplicates
-                           second = j;
-                           flag = true;
-                       }else{
-                           second = j;
-                       }
-                   }
-               }
-               //if it is the same number and the same index
-               //if it is half the target
-               if(first==second){
-                   continue;
-               }
-               return new int[]{first, second};
-           }
-       }
-       return new int[]{0,0};
+        return null;
     }
 }
