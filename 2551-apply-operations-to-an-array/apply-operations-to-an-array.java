8cd5df0732967bdc22eb9994e47e2cc2262942d3
Anurag Ganji <anuragganji09@gmail.com>
2024-08-09 23:38:20 -0700

    Time: 1 ms (95.78%) | Memory: 43.7 MB (47.51%) - LeetSync

--- 2551-apply-operations-to-an-array/apply-operations-to-an-array.java
+++ 2551-apply-operations-to-an-array/apply-operations-to-an-array.java
@@ -1,26 +1,35 @@
 class Solution {
     public int[] applyOperations(int[] nums) {
-        for(int i=0; i<nums.length-1;i++){
-            if(nums[i] == nums[i+1]){
-                nums[i] = nums[i]*2;
+        // First pass: Combine adjacent equal numbers and set the second number to 0
+        for(int i = 0; i < nums.length - 1; i++) {
+            // Check if the current number is equal to the next one
+            if(nums[i] == nums[i+1]) {
+                // Double the current number and set the next one to 0
+                nums[i] = nums[i] * 2;
                 nums[i+1] = 0;
             }
         }
-        int i=0, j=0;
-        while(i<nums.length && j<nums.length){
-            while(i<nums.length && nums[i]!=0){
+
+        // Second pass: Move all non-zero numbers to the front, preserving order
+        int i = 0, j = 0;
+        while(i < nums.length && j < nums.length) {
+            // Find the next 0 in the array
+            while(i < nums.length && nums[i] != 0) {
                 i++;
             }
-            while(j<nums.length && nums[j]==0){
+            // Find the next non-zero number after the last found zero
+            while(j < nums.length && nums[j] == 0) {
                 j++;
             }
-            if(i<nums.length && j<nums.length && i<j){
+            // If a 0 is found before a non-zero number, swap them
+            if(i < nums.length && j < nums.length && i < j) {
                 nums[i] = nums[j];
                 nums[j] = 0;
             }
-            j++;
+            j++;  // Move the second pointer forward
         }
+
+        // Return the modified array
         return nums;
-        
     }
 }
