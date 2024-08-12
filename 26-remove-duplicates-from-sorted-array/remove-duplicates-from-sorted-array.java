a9edc4c27879ef67d0f4165a6c1ad04e891f86e1
Anurag Ganji <anuragganji09@gmail.com>
2024-08-11 17:24:38 -0700

    Time: 0 ms (100.00%) | Memory: 44.7 MB (60.06%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -1,19 +1,11 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        // Initialize a pointer 'current' to track the position of the next unique element
         int current = 1;
-        
-        // Start iterating from the second element since the first element is always unique
-        for(int i = 1; i < nums.length; i++) {
-            // If the current element is the same as the previous, it's a duplicate
-            if(nums[i] == nums[i - 1]) {
-                continue; // Skip to the next iteration without doing anything
-            }
-            // If a new unique element is found, place it at the 'current' index
-            nums[current++] = nums[i];
+        for(int i=1; i<nums.length; i++){
+                if(nums[i]!=nums[i-1]){
+                    nums[current++] = nums[i];
+                }
         }
-        
-        // After processing, 'current' represents the number of unique elements
         return current;
     }
 }
