7a60a61c21c2e08c5024c656c9ad1a5414c1be7a
Anurag Ganji <anuragganji09@gmail.com>
2024-08-14 12:29:12 -0700

    Time: 6 ms (7.17%) | Memory: 43.8 MB (83.88%) - LeetSync

--- 2148-minimum-number-of-moves-to-seat-everyone/minimum-number-of-moves-to-seat-everyone.java
+++ 2148-minimum-number-of-moves-to-seat-everyone/minimum-number-of-moves-to-seat-everyone.java
@@ -1,14 +1,23 @@
 class Solution {
     public int minMovesToSeat(int[] seats, int[] students) {
-        // Sort the seats and students arrays to pair the closest available seat with each student
-        Arrays.sort(seats);
-        Arrays.sort(students);
-        // Initialize the variable to keep track of the total moves required
-        int count = 0;
-        for(int i = 0; i<seats.length; i++){
-            //Calculate the distance between the i-th seat and the i-th student
-            count += Math.abs(seats[i] - students[i]);
+        int[] positions = new int[100];
+        for(int i=0; i<seats.length; i++){
+            positions[seats[i]-1] += -1;
         }
+        for(int j=0; j<students.length; j++){
+            positions[students[j]-1] += 1;
+        }
+        int count = 0, empty = 0;
+        for(int i=0; i<100;i++){
+            if(positions[i] < 0){
+                empty+=positions[i];
+            }
+            if(positions[i] > 0){
+                empty+=positions[i];
+            }
+            count += Math.abs(empty)*1; 
+        }
+        System.out.println(Arrays.toString(positions));
         return count;
     }
 }
